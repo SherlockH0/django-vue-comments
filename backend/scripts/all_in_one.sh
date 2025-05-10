98f541d143c218a701355ecb81fe3c1c27f7bb27
SherlockH0 <ddmlf7@gmail.com>
2025-05-10 14:33:30 +0300

    Add support for render

---
+++ backend/scripts/all_in_one.sh
@@ -0,0 +1,7 @@
+scripts/run_celery.sh &
+
+echo "Running migrations..."
+poetry run python -m backend.manage migrate --no-input
+
+echo "Running daphne on port 80..."
+poetry run daphne -b 0.0.0.0 -p 80 backend.project.asgi:application
